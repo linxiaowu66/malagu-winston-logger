a322bd35d20386ffccbd6635bb3a05a162866698
linxiaowu66 <linguang661990@126.com>
2020-02-25 17:40:39 +0800

    feat(logger): finish

--- src/browser/module.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require(".");
-var core_1 = require("@malagu/core");
-exports["default"] = core_1.autoBind();
--- src/node/module.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var core_1 = require("@malagu/core");
-exports["default"] = core_1.autoBind();
