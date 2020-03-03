bff8e20f99353afd4681810e474056a321171ab9
linxiaowu66 <linguang661990@126.com>
2020-03-03 10:03:19 +0800

    fix(others): fix some little issues

--- src/node/logger/winston-logger.ts
+++ src/node/logger/winston-logger.ts
@@ -40,11 +40,11 @@ export class WinstonLogger implements Logger {
     this.logger.remove(transport);
   }
 
-  public getLogger() {
+  public getRawLogger() {
     return this.logger;
   }
 
-  messagePrefix() {
+  protected getMessagePrefix() {
     const hostname = os.hostname();
     const pid = process.pid;
     if (Context.getCurrent()) {
@@ -58,23 +58,23 @@ export class WinstonLogger implements Logger {
     return `(${pid} on ${hostname})`;
   }
 
-  error(message: string, prefix = this.messagePrefix()) {
+  error(message: string, prefix = this.getMessagePrefix()) {
     this.logger.error(`${prefix} ${message}`);
   }
 
-  warn(message: string, prefix = this.messagePrefix()) {
+  warn(message: string, prefix = this.getMessagePrefix()) {
     this.logger.warn(`${prefix} ${message}`);
   }
 
-  info(message: string, prefix = this.messagePrefix()) {
+  info(message: string, prefix = this.getMessagePrefix()) {
     this.logger.info(`${prefix} ${message}`);
   }
 
-  debug(message: string, prefix = this.messagePrefix()) {
+  debug(message: string, prefix = this.getMessagePrefix()) {
     this.logger.debug(`${prefix} ${message}`);
   }
 
-  verbose(message: string, prefix = this.messagePrefix()) {
+  verbose(message: string, prefix = this.getMessagePrefix()) {
     this.logger.verbose(`${prefix} ${message}`);
   }
 }
